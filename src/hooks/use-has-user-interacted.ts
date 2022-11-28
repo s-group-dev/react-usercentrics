5167a95570b6930932f62fc39c98e68e0c474c18
freyja-anh <anh@freyja.fi>
2022-11-28 15:17:37 +0200

    user context ping in the hook

--- src/hooks/use-has-user-interacted.ts
+++ src/hooks/use-has-user-interacted.ts
@@ -1,4 +1,16 @@
+import { useContext, useEffect, useState } from 'react'
+
+import { UsercentricsContext } from '../context.js'
 import { hasUserInteracted } from '../utils.js'
 
 /** Returns `true` if user has interacted with the dialog. */
-export const useHasUserInteracted = (): boolean => hasUserInteracted()
+export const useHasUserInteracted = (): boolean => {
+    const { ping } = useContext(UsercentricsContext)
+    const [userInteracted, setUserInteracted] = useState(hasUserInteracted())
+
+    useEffect(() => {
+        setUserInteracted(hasUserInteracted())
+    }, [ping])
+
+    return userInteracted
+}
