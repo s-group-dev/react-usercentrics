ed984cf29ce9c2d2f9b05154e6bc3fdb0736a1d3
freyja-anh <anh@freyja.fi>
2022-11-28 15:26:18 +0200

    useMemo instead of useEffect

--- src/hooks/use-has-user-interacted.ts
+++ src/hooks/use-has-user-interacted.ts
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useMemo } from 'react'
 
 import { UsercentricsContext } from '../context.js'
 import { hasUserInteracted } from '../utils.js'
@@ -6,11 +6,5 @@ import { hasUserInteracted } from '../utils.js'
 /** Returns `true` if user has interacted with the dialog. */
 export const useHasUserInteracted = (): boolean => {
     const { ping } = useContext(UsercentricsContext)
-    const [userInteracted, setUserInteracted] = useState(hasUserInteracted())
-
-    useEffect(() => {
-        setUserInteracted(hasUserInteracted())
-    }, [ping])
-
-    return userInteracted
+    return useMemo(hasUserInteracted, [ping])
 }
