2b43c8425a4bf86551c8812a5cdc667208fabd65
freyja-anh <anh@freyja.fi>
2022-11-28 14:18:16 +0200

    feat: hook to check if user has interacted with the dialog
    fix readme format

---
+++ src/hooks/use-has-user-interacted.ts
@@ -0,0 +1,4 @@
+import { hasUserInteracted } from '../utils.js'
+
+/** Returns `true` if user has interacted with the dialog. */
+export const useHasUserInteracted = (): boolean => hasUserInteracted()
