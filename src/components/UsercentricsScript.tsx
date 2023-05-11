09ed8468df67709401e1808088ced4a7278df9d8
Iiro Jäppinen <iiro.jappinen@sok.fi>
2023-05-11 21:37:33 +0300

    fix: pass script tag props through `UsercentricsScript` component

--- src/components/UsercentricsScript.tsx
+++ src/components/UsercentricsScript.tsx
@@ -1,7 +1,8 @@
 import type { FC } from 'react'
 import React from 'react'
 
-interface UsercentricsScriptProps {
+interface UsercentricsScriptProps
+    extends React.DetailedHTMLProps<React.ScriptHTMLAttributes<HTMLScriptElement>, HTMLScriptElement> {
     settingsId: string
     uiVersion?: string
     version?: 'production' | 'preview'
@@ -19,16 +20,24 @@ interface UsercentricsScriptProps {
  * @example <caption>Fixed UI version instead of latest</caption>
  * () => <UsercentricsScript settingsId="1234" uiVersion="3.21.1" />
  */
-export const UsercentricsScript: FC<UsercentricsScriptProps> = ({ settingsId, uiVersion = 'latest', version }) => (
+export const UsercentricsScript: FC<UsercentricsScriptProps> = ({
+    id = 'usercentrics-cmp',
+    settingsId,
+    uiVersion = 'latest',
+    version,
+    ...rest
+}) => (
     <script
-        async
+        {...rest}
+        async={'async' in rest ? rest.async : true}
         data-settings-id={settingsId}
         data-version={version}
-        id="usercentrics-cmp"
+        id={id}
         src={`https://app.usercentrics.eu/browser-ui/${uiVersion}/loader.js`}
     />
 )
 
 UsercentricsScript.defaultProps = {
+    id: 'usercentrics-cmp',
     uiVersion: 'latest',
 }
