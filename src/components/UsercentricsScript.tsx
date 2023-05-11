ea076a61a7a3992cb602a31235d57cc6c900fa65
Iiro Jäppinen <iiro.jappinen@sok.fi>
2023-05-11 21:34:38 +0300

    feat: add `uiVersion?: string` prop to `UsercentricsScript` component

--- src/components/UsercentricsScript.tsx
+++ src/components/UsercentricsScript.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 interface UsercentricsScriptProps {
     settingsId: string
+    uiVersion?: string
     version?: 'production' | 'preview'
 }
 
@@ -14,13 +15,20 @@ interface UsercentricsScriptProps {
  *
  * @example <caption>Preview mode for development</caption>
  * () => <UsercentricsScript settingsId="1234" version="preview" />
+ *
+ * @example <caption>Fixed UI version instead of latest</caption>
+ * () => <UsercentricsScript settingsId="1234" uiVersion="3.21.1" />
  */
-export const UsercentricsScript: FC<UsercentricsScriptProps> = ({ settingsId, version }) => (
+export const UsercentricsScript: FC<UsercentricsScriptProps> = ({ settingsId, uiVersion = 'latest', version }) => (
     <script
         async
         data-settings-id={settingsId}
         data-version={version}
         id="usercentrics-cmp"
-        src="https://app.usercentrics.eu/browser-ui/latest/loader.js"
+        src={`https://app.usercentrics.eu/browser-ui/${uiVersion}/loader.js`}
     />
 )
+
+UsercentricsScript.defaultProps = {
+    uiVersion: 'latest',
+}
