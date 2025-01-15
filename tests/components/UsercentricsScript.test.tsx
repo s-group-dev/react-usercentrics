f1e50bf330f19bc2b8c7da78560d06be8cb3357c
Iiro Jäppinen <iiro.jappinen@sok.fi>
2025-01-15 09:00:14 +0200

    test: use node "react-dom/server" to avoid Testing Library issue

--- tests/components/UsercentricsScript.test.tsx
+++ tests/components/UsercentricsScript.test.tsx
@@ -1,5 +1,7 @@
-import { render } from '@testing-library/react'
+/** @jest-environment node */
+
 import React from 'react'
+import { renderToStaticMarkup } from 'react-dom/server'
 
 import { UsercentricsScript } from '../../src/components/UsercentricsScript.js'
 
@@ -7,42 +9,34 @@ describe('Usercentrics', () => {
     describe('components', () => {
         describe('UsercentricsScript', () => {
             it('should render basic props', () => {
-                const { container } = render(<UsercentricsScript settingsId="1234" />)
+                const result = renderToStaticMarkup(<UsercentricsScript settingsId="1234" />)
 
-                expect(container.firstChild).toMatchInlineSnapshot(`
-                    <script
-                      async=""
-                      data-settings-id="1234"
-                      id="usercentrics-cmp"
-                      src="https://app.usercentrics.eu/browser-ui/latest/loader.js"
-                    />
-                `)
-            })
-
-            it('should render preview attribute', () => {
-                const { container } = render(<UsercentricsScript settingsId="1234" version="preview" />)
-
-                expect(container.firstChild).toHaveAttribute('data-version', 'preview')
-            })
-
-            it('should allow disabling default async prop', () => {
-                const { container } = render(<UsercentricsScript settingsId="1234" async={undefined} defer />)
-
-                expect(container.firstChild).not.toHaveAttribute('async')
-                expect(container.firstChild).toHaveAttribute('defer', '')
-            })
-
-            it('should allow specifying UI version', () => {
-                const { container } = render(<UsercentricsScript settingsId="1234" uiVersion="3.24.0" />)
-
-                expect(container.firstChild).toHaveAttribute(
-                    'src',
-                    'https://app.usercentrics.eu/browser-ui/3.24.0/loader.js',
+                expect(result).toMatchInlineSnapshot(
+                    `"<script async="" data-settings-id="1234" id="usercentrics-cmp" src="https://app.usercentrics.eu/browser-ui/latest/loader.js"></script>"`,
                 )
             })
 
+            it('should render preview attribute', () => {
+                const result = renderToStaticMarkup(<UsercentricsScript settingsId="1234" version="preview" />)
+
+                expect(result).toMatch('data-version="preview"')
+            })
+
+            it('should allow disabling default async prop', () => {
+                const result = renderToStaticMarkup(<UsercentricsScript settingsId="1234" async={undefined} defer />)
+
+                expect(result).not.toMatch('async')
+                expect(result).toMatch('defer')
+            })
+
+            it('should allow specifying UI version', () => {
+                const result = renderToStaticMarkup(<UsercentricsScript settingsId="1234" uiVersion="3.24.0" />)
+
+                expect(result).toMatch('src="https://app.usercentrics.eu/browser-ui/3.24.0/loader.js"')
+            })
+
             it('should allow passing integrity prop', () => {
-                const { container } = render(
+                const result = renderToStaticMarkup(
                     <UsercentricsScript
                         settingsId="1234"
                         uiVersion="3.24.0"
@@ -50,14 +44,13 @@ describe('Usercentrics', () => {
                     />,
                 )
 
-                expect(container.firstChild).toHaveAttribute(
-                    'integrity',
-                    'sha384-WRloNuM/QNkzJ4GkUAZgJ5CWgTFhVjsLKVQbACSHGOifUvw2WJk1QaY9mphkn96U',
+                expect(result).toMatch(
+                    'integrity="sha384-WRloNuM/QNkzJ4GkUAZgJ5CWgTFhVjsLKVQbACSHGOifUvw2WJk1QaY9mphkn96U"',
                 )
             })
 
             it('should not allow the src prop', () => {
-                const { container } = render(
+                const result = renderToStaticMarkup(
                     <UsercentricsScript
                         settingsId="1234"
                         /** @ts-expect-error: Type 'string' is not assignable to type 'undefined'. */
@@ -65,10 +58,7 @@ describe('Usercentrics', () => {
                     />,
                 )
 
-                expect(container.firstChild).toHaveAttribute(
-                    'src',
-                    'https://app.usercentrics.eu/browser-ui/latest/loader.js',
-                )
+                expect(result).toMatch('src="https://app.usercentrics.eu/browser-ui/latest/loader.js"')
             })
         })
     })
